3b307b39716012472634ff69de7d955a3bea2e92
Christiaan <christiaan.does@student.graafschapcollege.nl>
2021-04-28 14:35:34 +0200

    Test

---
+++ casus4/footer.php
@@ -0,0 +1,23 @@
+<footer>
+        <div class="container">
+            <div class="column column1">
+                <h3>Contact</h3>
+                <p>Adres: Doetichem</p>
+                <p>Telefoon: 06-12345678</p>
+                <p>Email: john@doe.nl</p>
+            </div>
+            <div class="column column2">
+                <h3>Links</h3>
+                <ul class="footerMenu">
+                <li><a href="<?php echo PUBLIC_PATH ; ?>/index.php">Home</a></li>
+            <li><a href="<?php echo PUBLIC_PATH ; ?>/info.php">Info</a></li>
+            <li><a href="<?php echo PUBLIC_PATH ; ?>/reserveren.php">Reserveren</a></li>
+            
+                </ul>
+            </div>
+        </div>
+
+    </footer>
+    <!--  End footer -->
+    </body>
+</html>
