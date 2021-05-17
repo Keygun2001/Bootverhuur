15233c31794312f091638b8ab4744f48e5cfaf1a
duncan-development <nietvanbelang2@gmail.com>
2021-05-17 14:08:26 +0200

    Dikke push 2

--- public/server.php
+++ public/server.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 include('../src/databaseFunctions.php');
 include('../src/userFunctions.php');
 session_start();
@@ -29,34 +28,5 @@ if(isset($_POST['saveorder'])){
     $_SESSION['tempOrderID'] = $newOrder;
     header("Location:orderConfirmation.php");
 }
-=======
-    include('../src/databaseFunctions.php');
-    include('../src/userFunctions.php');
-    session_start();
-    $firstName="";
-    $lastName="";
-    $phonenumber="";
-    $email="";
-    $password="";
-    $bootID = "";
-    $dagdeelID = "";
-    $id = 0;
-
-    if(isset($_POST['saveorder'])){
-        $firstName=$_POST['firstName'];
-        $lastName=$_POST['lastName'];
-        $phonenumber=$_POST['phoneNumber'];
-        $email=$_POST['email'];
-        $password=$_POST['passWord'];
-        $bootID = $_POST['bootSelect'];
-        $dagdeelID = $_POST['dagdeelSelect'];
-        $date = $_POST['DatumSelect'];
-        
-        $newOrder = registerUser($firstName,$lastName,$phonenumber,$email,$password, $bootID, $dagdeelID,$date);
-        //die($newOrder);
-        $_SESSION['tempOrderID'] = $newOrder;
-        header("Location:orderConfirmation.php");
-    }
->>>>>>> 42adab8e2c25114b5abc61bea2c1167685fba88d
     
 ?>
