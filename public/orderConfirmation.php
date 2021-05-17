093ba0856d273b3a8e0457e27aa83d65c4753b6a
duncan-development <nietvanbelang2@gmail.com>
2021-05-17 14:07:17 +0200

    Dikke push 2

--- public/orderConfirmation.php
+++ public/orderConfirmation.php
@@ -1,10 +1,7 @@
 <?php
-<<<<<<< HEAD
 
   include('server.php');
   include("../config/database.php");
-=======
->>>>>>> 42adab8e2c25114b5abc61bea2c1167685fba88d
   include('header.php');
   include('server.php');
   include_once('../src/databaseFunctions.php');
@@ -15,7 +12,6 @@
   $order = db_getData("SELECT * FROM verhuur WHERE id=$orderID");
   $order = $order->fetch_array();
 
-<<<<<<< HEAD
     $bootID = $order['BootID'];
     $boot = db_getData("SELECT * FROM Boot WHERE id=$bootID");
     $boot = $boot->fetch_array();
@@ -29,19 +25,10 @@
     $optie = $optie->fetch_array();
     
   //die(print_r($dagdeel));
-=======
 
-  $bootID = $order['BootID'];
-  $boot = db_getData("SELECT * FROM boot WHERE id=$bootID");
-  $boot = $boot->fetch_array();
-
-  $dagID = $order['DagID'];
-  $dagdeel = db_getData("SELECT * FROM dagdeel WHERE id=$dagID");
-  $dagdeel = $dagdeel->fetch_array();
->>>>>>> 42adab8e2c25114b5abc61bea2c1167685fba88d
+  
 ?>
 <body>
-<<<<<<< HEAD
 <h1>Bedankt voor het huren van <?php echo "Deze boot " .$boot['BoatName']; ?> </h1>
 <h2>Dagdeel: <?php echo $dagdeel['DayPart'] ?></h2>
 <h2>Datum: <?php echo $order['DateVerhuur']; ?></h2>
@@ -71,43 +58,6 @@ else{
 <!-- <p>Wachtwoord </p> -->
 
     
-=======
-    <h1>
-        Bedankt voor het huren van <?php echo "Deze boot " .$boot['BoatName']; ?> 
-    </h1>
-    <h2>
-        Dagdeel: <?php echo $dagdeel['DayPart'] ?>
-    </h2>
-    <h2>
-        Datum: <?php echo $order['DateVerhuur']; ?>
-    </h2>
-    <h1>
-        Uw Gegevens
-    </h1>
-    <p>
-        Voornaam:  <?php echo $order['FirstName']; ?>
-    </p>
-    <p>
-        Achternaam:  <?php echo $order['LastName']; ?>
-    </p>
-    <p>
-        Telefoon Nummer:  <?php echo $order['PhoneNumber']; ?>
-    </p>
-    <p>
-        Email: <?php echo $order['Email']; ?>
-    </p>
-    <p>
-        Prijs:€ <?php  if($dagdeel['DayPart'] == "Hele Dag") 
-        { 
-            echo  15* $boot['Price']; 
-        }
-        else{
-            echo 5 * $boot['Price'];
-        }
-
-        ?>
-    </p>
->>>>>>> 42adab8e2c25114b5abc61bea2c1167685fba88d
 </body>
 
 <?php 
